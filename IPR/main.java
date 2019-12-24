742e04c0ae6c882c146af11998e64cddc89dd102
Ilyas <ilyas.sarsenbayev@nu.edu.kz>
2019-12-24 13:35:38 +0600

    resume

---
+++ IPR/main.java
@@ -0,0 +1,9 @@
+class Teacher {
+	public static void main(String[] args) {
+		String name = "Ilyas";
+		String surname = "Sarsenbayev";
+		int age = 25;
+		char gender = 'M';
+		boolean senseOfHumor = true;
+	}
+}
